8a5f9ea168759d84733795101b719e6d23a595a9
katlego <katlego@accsight.org>
2022-11-28 23:29:53 +0200

    added views, defined routes and controller templates

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -2,8 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 var homeCtrl = require('../controllers/home');
+const mCtrl = require('../controllers/Ms')
 
 /* GET home page. */
 router.get('/', homeCtrl.home);
+
+//model routes
+//list
+router
+  .route('/ms')
+  .get(mCtrl.readMs)
+  .post(mCtrl.createM);
+//instances
+router
+  .route('/ms/:mId')
+  .get(mCtrl.readM)
+  .put(mCtrl.updateM)
+  .delete(mCtrl.deleteM);
+
+//submodel routes
+//list
+router
+  .route('/ms/:mId/sms')
+  .get(mCtrl.readSMs)
+  .post(mCtrl.createSM);
+//instance
+router
+  .route('/ms/:mId/sms/:smId')
+  .get(mCtrl.readSM)
+  .put(mCtrl.updateSM)
+  .delete(mCtrl.deleteSM);
+
 
 module.exports = router;
