15045817838823bd7ad2afbfe767f3df90e543d5
katlego <katlego@accsight.org>
2022-11-29 05:46:09 +0200

    boiler 1

--- app_server/controllers/Ms.js
+++ app_server/controllers/Ms.js
@@ -29,49 +29,10 @@ const deleteM = (req, res) => {
 };
 
 
-
-//submodel list operations
-const createSM = (req, res) => {
-    res.render('sm-form', {
-        title: 'List of Ms'
-    });
-};
-
-const readSMs = (req, res) => {
-    res.render('sm-list', {
-        title: 'Details of SMs'
-    });
-};
-
-//submodel instance operations
-const readSM = (req, res) => {
-    res.render('sm-details', {
-        title: 'Details of SM'
-    });
-};
-const updateSM = (req, res) => {
-    res.render('sm-update', {
-        title: 'Update SM'
-    });
-};
-const deleteSM = (req, res) => {
-    res.render('sm-details', {
-        title: 'Delete SM'
-    });
-};
-
-
-
-
 module.exports = {
     createM,
     readMs,
     readM,
     updateM,
     deleteM,
-    createSM,
-    readSMs,
-    readSM,
-    updateSM,
-    deleteSM,
 };
