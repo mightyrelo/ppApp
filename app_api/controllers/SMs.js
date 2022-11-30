2620802657e040b432a75e5de3ef3d473fd9ebf2
katlego <katlego@accsight.org>
2022-11-30 20:52:01 +0200

    useless

--- app_api/controllers/SMs.js
+++ app_api/controllers/SMs.js
@@ -22,28 +22,49 @@ const smReadAll = (req, res) => {
         if(!m) {sendJSONResponse(res, 404, {"message":"m not found"});}
         const smS = m.sms;
         const response = {
-            k: m.a1,
-            j: smS
+            m: {
+                a1: m.a1,
+                id: m._id
+
+            },
+            sms: smS
         };
         sendJSONResponse(res, 200, response);
      });
 
 };
 
+const getSM = (req, res, m) => {
+    for(let i = 0; i < m.sms.length; i++) {
+        console.log(m.sms[i]);
+    }
+}
+
 
 
 //submodel instance operations
 const smReadOne = (req, res) => {
-    if(!req.params.mId || !req.params.smId) {sendJSONResponse(res, 404, {"message":"both parameters required"})}
+    if(!req.params.mId || !req.params.smId) {sendJSONResponse(res, 404, {"message":"both parameters required"}); return}
     M
      .findById(req.params.mId)
+     .select('a1 sms')
      .exec((err, m) => {
-        if(err) {sendJSONResponse(res, 400, err);}
-        if(!m) {sendJSONResponse(res, 404, {"message":"m not found"});}
-        const sm = m.sms.id(req.params.smId);
+        if(err) {sendJSONResponse(res, 400, err); return}
+        if(!m) {sendJSONResponse(res, 404, {"message":"m not found"}); return}
+        if(!m.sms || m.sms.length === 0 ) {sendJSONResponse(res, 404, {"message":"no sms found"}); return}
+        //console.log(m.sms);
+        const sm = m.sms.id(req.params.smId); 
+        //console.log(sm); 
+        for(let i = 0; i < m.sms.length; i++) {
+            console.log(sm.sms._id, req.params.smId);
+        } 
+        //console.log(m);
         const response = {
-            k: m.a1,
-            j: sm
+            m: {
+               a1: m.a1,
+               id: m._id
+            },
+            sm
         };
         sendJSONResponse(res, 200, response);
      })
