32567852e8c4b7ea1d83dcafd825077fa9945c54
katlego <katlego@accsight.org>
2022-11-30 10:02:48 +0200

    resolved merge issue locally

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -1,5 +1,7 @@
 const home = (req, res, next) => {
-    res.render('index', { title: 'Katlego' });
+    res.render('index', { title: 'ppApp',
+      strap: 'The one app that helps you put pap on the table.' 
+ });
 }
 
 module.exports = {
