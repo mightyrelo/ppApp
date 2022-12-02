1d435d68fd7b8dee32aa62bd04087effca602a1e
katlego <katlego@accsight.org>
2022-12-02 13:20:40 +0200

    added home and list route

--- app_public/src/app/app.module.ts
+++ app_public/src/app/app.module.ts
@@ -1,21 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
 
 import { MListComponent } from './m-list/m-list.component';
 import { AppendSPipe } from './append-s.pipe';
+import { FrameworkComponent } from './framework/framework.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
     MListComponent,
-    AppendSPipe
+    AppendSPipe,
+    FrameworkComponent,
+    NavBarComponent,
+    FooterComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([{
+      path: 'ms',
+      component: MListComponent
+    }])
   ],
   providers: [],
-  bootstrap: [MListComponent]
+  bootstrap: [FrameworkComponent]
 })
 export class AppModule { }
